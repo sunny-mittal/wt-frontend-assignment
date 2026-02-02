24f08a9d1cc88f3e8d0e2d8c8b7e8435c27ef211
Sunny Mittal <sunnymittal2003@gmail.com>
2026-02-02 00:08:46 -0700

    Add delete method and function

--- src/components/MemberForm.tsx
+++ src/components/MemberForm.tsx
@@ -5,6 +5,11 @@ import {
   Stack,
   TextField,
   Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from "@mui/material"
 import {
   type CreateMemberDTO,
@@ -15,6 +20,8 @@ import { useForm, Controller } from "react-hook-form"
 import { useNavigate } from "react-router-dom"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { ApiClient } from "../api/client"
+import { useState } from "react"
+import { getFullName } from "../utils/string"
 
 type Props = {
   initial?: Member
@@ -23,6 +30,7 @@ type Props = {
 export function MemberForm({ initial }: Props) {
   const navigate = useNavigate()
   const client = useQueryClient()
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false)
   const { control, handleSubmit } = useForm<CreateMemberDTO | UpdateMemberDTO>({
     defaultValues: {
       firstName: initial?.firstName ?? "",
@@ -56,7 +64,17 @@ export function MemberForm({ initial }: Props) {
     // onError() {},
   })
 
-  const apiFunction = handleSubmit((values) => {
+  const deleteMutation = useMutation({
+    mutationFn(id: string) {
+      return ApiClient.deleteMember(id)
+    },
+    onSuccess() {
+      client.invalidateQueries({ queryKey: ["members"] })
+      navigate("/")
+    },
+  })
+
+  const upsertFunction = handleSubmit((values) => {
     if (initial) {
       return updateMutation.mutate({ id: initial.id, member: values })
     }
@@ -65,84 +83,119 @@ export function MemberForm({ initial }: Props) {
   })
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        mt: 4,
-      }}
-    >
-      <Box display="flex" justifyContent="space-between" minWidth={640}>
-        <Typography variant="h4" color="primary" sx={{ mb: 2 }}>
-          {initial ? "Update" : "Create"} Member
-        </Typography>
-        <Button onClick={() => navigate("/")}>Back to List</Button>
-      </Box>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          mt: 4,
+        }}
+      >
+        <Box display="flex" justifyContent="space-between" minWidth={640}>
+          <Typography variant="h4" color="primary" sx={{ mb: 2 }}>
+            {initial ? "Update" : "Create"} Member
+          </Typography>
+          <Button onClick={() => navigate("/")}>Back to List</Button>
+        </Box>
 
-      <Box onSubmit={apiFunction} component="form" minWidth={640}>
-        <Stack direction="column" spacing={3}>
-          <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+        <Box onSubmit={upsertFunction} component="form" minWidth={640}>
+          <Stack direction="column" spacing={3}>
+            <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+              <Controller
+                control={control}
+                name="firstName"
+                render={({ field }) => {
+                  return <TextField fullWidth {...field} label="First Name" />
+                }}
+              />
+              <Controller
+                control={control}
+                name="lastName"
+                render={({ field }) => {
+                  return <TextField fullWidth {...field} label="Last Name" />
+                }}
+              />
+            </Stack>
             <Controller
               control={control}
-              name="firstName"
+              name="dateOfBirth"
               render={({ field }) => {
-                return <TextField fullWidth {...field} label="First Name" />
+                return <TextField fullWidth {...field} type="date" />
               }}
             />
             <Controller
               control={control}
-              name="lastName"
+              name="sex"
               render={({ field }) => {
-                return <TextField fullWidth {...field} label="Last Name" />
+                return (
+                  <TextField {...field} select>
+                    <MenuItem value="male">Male</MenuItem>
+                    <MenuItem value="female">Female</MenuItem>
+                    <MenuItem value="other">Other</MenuItem>
+                  </TextField>
+                )
+              }}
+            />
+            <Controller
+              control={control}
+              name="status"
+              render={({ field }) => {
+                return (
+                  <TextField {...field} select>
+                    <MenuItem value="ACTIVE">Active</MenuItem>
+                    <MenuItem value="PAUSED">Paused</MenuItem>
+                  </TextField>
+                )
               }}
             />
           </Stack>
-          <Controller
-            control={control}
-            name="dateOfBirth"
-            render={({ field }) => {
-              return <TextField fullWidth {...field} type="date" />
-            }}
-          />
-          <Controller
-            control={control}
-            name="sex"
-            render={({ field }) => {
-              return (
-                <TextField {...field} select>
-                  <MenuItem value="male">Male</MenuItem>
-                  <MenuItem value="female">Female</MenuItem>
-                  <MenuItem value="other">Other</MenuItem>
-                </TextField>
-              )
-            }}
-          />
-          <Controller
-            control={control}
-            name="status"
-            render={({ field }) => {
-              return (
-                <TextField {...field} select>
-                  <MenuItem value="ACTIVE">Active</MenuItem>
-                  <MenuItem value="PAUSED">Paused</MenuItem>
-                </TextField>
-              )
-            }}
-          />
-        </Stack>
-        <Box sx={{ mt: 2 }} display="flex" justifyContent="space-between">
-          {!!initial && (
-            <Button variant="contained" color="error">
-              Delete Member
+          <Box sx={{ mt: 2 }} display="flex" justifyContent="space-between">
+            {!!initial && (
+              <Button
+                onClick={() => setShowDeleteDialog(true)}
+                variant="contained"
+                color="error"
+              >
+                Delete Member
+              </Button>
+            )}
+            <Button type="submit" variant="contained" color="primary">
+              {initial ? "Update" : "Create"} Member
             </Button>
-          )}
-          <Button type="submit" variant="contained" color="primary">
-            {initial ? "Update" : "Create"} Member
-          </Button>
+          </Box>
         </Box>
       </Box>
-    </Box>
+      {!!initial && (
+        <Dialog
+          open={showDeleteDialog}
+          onClose={() => setShowDeleteDialog(false)}
+        >
+          <DialogTitle>Delete {getFullName(initial)}?</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Deletion is irreversible. Make sure this is what you want to do!
+            </DialogContentText>
+            <DialogActions>
+              <Button
+                color="secondary"
+                onClick={() => setShowDeleteDialog(false)}
+              >
+                Cancel
+              </Button>
+              <Button
+                onClick={() => {
+                  deleteMutation.mutate(initial.id)
+                }}
+                color="primary"
+              >
+                Confirm
+              </Button>
+            </DialogActions>
+          </DialogContent>
+        </Dialog>
+      )}
+    </>
   )
 }
